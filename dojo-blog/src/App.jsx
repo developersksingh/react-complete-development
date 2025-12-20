09b6500e86b130d846bae7ae2b67aff39c55652c
developersksingh <sanjay.developer@codunite.in>
2025-12-20 21:53:16 +0530

    basic ract terms revised
    basic ract terms revised

--- dojo-blog/src/App.jsx
+++ dojo-blog/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+ const title = "Welcome to the my new react blog.";
+ const link ="www.google.com";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+    <h1>{title} </h1>
+    <p>{"Hello ,World"}</p>
+    <a href={link} title={link}>Visit Site</a>
+    <p>{[1,2,3,4,5]}</p>
+    <p>{Math.random()*100}</p>
+    <p>Active User : {100+268}</p>
+
+  </>;
 }
 
-export default App
+export default App;
