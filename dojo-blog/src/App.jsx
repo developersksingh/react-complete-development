608dc1f56ab77073ab13afacd656b29b0eafdf0a
developersksingh <sanjay.developer@codunite.in>
2025-12-20 22:02:53 +0530

    basic mutiple component
    basic mutiple component

--- dojo-blog/src/App.jsx
+++ dojo-blog/src/App.jsx
@@ -2,20 +2,29 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from "./Navbar";
 
 function App() {
- const title = "Welcome to the my new react blog.";
- const link ="www.google.com";
+  const title = "Welcome to the my new react blog.";
+  const link = "www.google.com";
 
-  return <>
-    <h1>{title} </h1>
-    <p>{"Hello ,World"}</p>
-    <a href={link} title={link}>Visit Site</a>
-    <p>{[1,2,3,4,5]}</p>
-    <p>{Math.random()*100}</p>
-    <p>Active User : {100+268}</p>
-
-  </>;
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-success">{title} </h1>
+        <p>{"Hello ,World"}</p>
+        <a href={link} title={link}>
+          Visit Site
+        </a>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 100}</p>
+        <p>Active User : {100 + 268}</p>
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ dojo-blog/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <h1>Blog World</h1>
+    <div>Navbar</div>
+    </>
+  )
+}
+
+export default Navbar
